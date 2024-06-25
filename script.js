62973bad19bad895937295165897549d6a621ab5
Diprati Das <102643011+developer0311@users.noreply.github.com>
2024-06-25 23:46:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -102,21 +102,22 @@ const typed = new Typed(".multiple-text", {
 
 const circles = document.querySelectorAll(".circle");
 
-circles.forEach((elem) => {
-  var dots = elem.getAttribute("data-dots");
-  var marked = elem.getAttribute("data-percent");
-  var percent = Math.floor((dots * marked) / 100);
-  var points = "";
-  var rotate = 360 / dots;
+    circles.forEach((element) => {
+        let dots = element.getAttribute("data-dots");
+        let marked = element.getAttribute("data-percent");
 
-  for (let i = 0; i < dots; i++) {
-    points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
-  }
-  elem.innerHTML = points;
+        let percent = Math.floor((dots * marked) / 100);
+        let points = "";
+        let rotate = 360 / dots;
 
-  const pointsMarked = elem.querySelectorAll(".points");
+        for (let i = 0; i < dots; i++) {
+            points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+        }
 
-  for (let i = 0; i < percent; i++) {
-    pointsMarked[i].classList.add("marked");
-  }
-});
+        element.innerHTML = points;
+
+        const pointsMarked = element.querySelectorAll(".points");
+        for (let i = 0; i < percent; i++) {
+            pointsMarked[i].classList.add("marked");
+        }
+    });
